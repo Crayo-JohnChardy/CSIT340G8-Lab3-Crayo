bba987df67474ae25a7ee3a7f30f028047a1c821
Chardy2610 <johnchardycrayo@gmail.com>
2026-09-21 22:15:29 +0800

    1.3: course information step 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} - {props.exercise} units
+      {props.name} - {props.exercises} units
     </p>
   )
 }
@@ -13,9 +13,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part name={props.part1.name} exercises={props.part1.exercises} />
+      <Part name={props.part2.name} exercises={props.part2.exercises} />
+      <Part name={props.part3.name} exercises={props.part3.exercises} />
     </div>
   )
 }
@@ -35,12 +35,19 @@ const Footer = (props) => {
 
 const App = () => {
   const course = 'CSIT340 - Industry Elective 1'
-  const part1 = 'IT317 - Project Management for IT'
-  const exercises1 = 3
-  const part2 = 'IT365 - Data Analytics 1'
-  const exercises2 = 3
-  const part3 = 'CSIT327 - Information Management 2'
-  const exercises3 = 3
+  
+  const part1 = {
+    name: 'IT317 - Project Management for IT',
+    exercises: 3
+  }
+  const part2 = {
+    name: 'IT365 - Data Analytics 1',
+    exercises: 3
+  }
+  const part3 = {
+    name: 'CSIT327 - Information Management 2',
+    exercises: 3
+  }
 
   const studentInfo = {
     fullName: 'John Chardy Crayo',
@@ -51,12 +58,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
       <Footer 
         fullName={studentInfo.fullName} 
         courseCode={studentInfo.courseCode} 
